a2f90adbf5901793dd6259ae5b29d04860f7a93b
HimanshuJ-dev <himanshu.rkj3@gmail.com>
2023-09-27 19:44:40 +0530

    updated the website so only the cardlist scrolls now instead of whole website

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import CardList from './CardList';
 import SearchBox from './SearchBox';
+import Scroll from './scroll';
 import './App.css';
 
 class App extends React.Component {
@@ -40,7 +41,9 @@ class App extends React.Component {
                 <div className="tc">
                     <h1 className="f1">RoboFriends</h1>
                     <SearchBox searchChange={this.onSearchChange} />
-                    <CardList robots={filteredrobots} />
+                    <Scroll>
+                        <CardList robots={filteredrobots} />
+                    </Scroll>
                 </div>
             );
         }
