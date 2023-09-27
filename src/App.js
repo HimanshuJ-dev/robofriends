85fca1d634f81c0cdc3299d26818c6433a9981ea
HimanshuJ-dev <himanshu.rkj3@gmail.com>
2023-09-27 19:09:33 +0530

    changed the app file to fetch user details from jsonplaceholder.com instead of hardcoded files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import CardList from './CardList';
-import { robots } from './robots';
 import SearchBox from './SearchBox';
 import './App.css';
 
@@ -8,9 +7,15 @@ class App extends React.Component {
     constructor() {
         super()
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ''
         }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => this.setState({robots: users}))
     }
 
     onSearchChange = (event) => {
@@ -22,13 +27,23 @@ class App extends React.Component {
         const filteredrobots = this.state.robots.filter(robots => {
             return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
         })
-        return (
-            <div className="tc">
-                <h1 className="f1">RoboFriends</h1>
-                <SearchBox searchChange={this.onSearchChange} />
-                <CardList robots={filteredrobots} />
-            </div>
-        );
+        if (this.state.robots.length === 0) {
+            return (
+                <div className="tc">
+                    <h1 className="f1">RoboFriends</h1>
+                    <SearchBox searchChange={this.onSearchChange} />
+                    <h1>Loading...</h1>
+                </div>
+            );
+        } else {
+            return (
+                <div className="tc">
+                    <h1 className="f1">RoboFriends</h1>
+                    <SearchBox searchChange={this.onSearchChange} />
+                    <CardList robots={filteredrobots} />
+                </div>
+            );
+        }
     }
 }
 
