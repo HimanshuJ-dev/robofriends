720b1610f26a961a86201d7e5ad88dcf0a1bddbf
HimanshuJ-dev <himanshu.rkj3@gmail.com>
2023-09-27 20:02:14 +0530

    did some code optimisation

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,7 @@
 import React from "react";
-import CardList from './CardList';
-import SearchBox from './SearchBox';
-import Scroll from './scroll';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/scroll';
 import './App.css';
 
 class App extends React.Component {
@@ -16,28 +16,27 @@ class App extends React.Component {
     componentDidMount() {
         fetch('https://jsonplaceholder.typicode.com/users')
             .then(response => response.json())
-            .then(users => this.setState({robots: users}))
+            .then(users => this.setState({ robots: users }))
     }
 
     onSearchChange = (event) => {
-        this.setState({searchfield: event.target.value})
-        
+        this.setState({ searchfield: event.target.value })
+
     }
 
     render() {
-        const filteredrobots = this.state.robots.filter(robots => {
-            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        const { robots, searchfield } = this.state;
+        const filteredrobots = robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
         })
-        if (this.state.robots.length === 0) {
-            return (
-                <div className="tc">
-                    <h1 className="f1">RoboFriends</h1>
-                    <SearchBox searchChange={this.onSearchChange} />
-                    <h1>Loading...</h1>
-                </div>
-            );
-        } else {
-            return (
+        return !robots.length ? (
+            <div className="tc">
+                <h1 className="f1">RoboFriends</h1>
+                <SearchBox searchChange={this.onSearchChange} />
+                <h1>Loading...</h1>
+            </div>
+        )
+            : (
                 <div className="tc">
                     <h1 className="f1">RoboFriends</h1>
                     <SearchBox searchChange={this.onSearchChange} />
@@ -46,7 +45,6 @@ class App extends React.Component {
                     </Scroll>
                 </div>
             );
-        }
     }
 }
 
